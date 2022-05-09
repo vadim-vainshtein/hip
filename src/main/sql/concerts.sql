9a5f0517148e7643e1ac009abfbf5651661e8154
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2022-05-09 18:18:55 +0300

    created a plan for writing info into tables

--- src/main/sql/concerts.sql
+++ src/main/sql/concerts.sql
@@ -14,8 +14,8 @@ CREATE TABLE performers
 CREATE TABLE places
 (
 	place_id	INT NOT NULL AUTO_INCREMENT,
-	place_name	VARCHAR(255) NOT NULL,
-	place_address	VARCHAR(255) NOT NULL,
+	place_name	VARCHAR(255),
+	place_address	VARCHAR(255),
 	PRIMARY KEY (place_id)
 );
 
@@ -25,11 +25,16 @@ CREATE TABLE concerts
         date_time       DATETIME NOT NULL,
         place_id        INT,
         program_name   VARCHAR(255),
-        link		VARCHAR(255),
+        link		VARCHAR(1024),
         PRIMARY KEY (concert_id),
         FOREIGN KEY (place_id) REFERENCES places (place_id) ON DELETE SET NULL
 );
 
+CREATE TABLE instruments
+(
+	instrument_id		INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	instrument_name	VARCHAR(255) NOT NULL
+);	
 
 CREATE TABLE performers_concerts
 (
@@ -39,4 +44,10 @@ CREATE TABLE performers_concerts
 	FOREIGN KEY (concert_id) REFERENCES concerts (concert_id)
 );
 
-
+CREATE TABLE performers_instruments
+(
+	performer_id	INT,
+	instrument_id	INT,
+	FOREIGN KEY (performer_id) REFERENCES performers (performer_id),
+	FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id)
+);
